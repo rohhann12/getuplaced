5d322d4686d142bea687930a99a9c0ac21fdd6d9
Rohan <rsharma7_be22@thapar.edu>
2025-04-17 15:37:45 +0530

    commit getting too big

--- app/utils/emailFinder.ts
+++ app/utils/emailFinder.ts
@@ -1,35 +1,43 @@
 import prisma from "./db";
-export async function Email(){
-    let arr2:any=[]; 
-    function getRandomTen(arr: { id: string; userId: string; FounderId: string; isSent: boolean; }[]) {
-        const shuffled = [...arr];
-        for (let i = shuffled.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-        }
-        arr2=shuffled.slice(0,10)
-        return arr2;
+
+export async function Email(arr: any[]) {
+  function getRandomTen(arr: { id: string; userId: string; founderId: string; isSent: boolean; }[]) {
+    const shuffled = [...arr];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-    // we have to find emails of founders jinko mail ni bheji and we are sending them email
-    const finder=await prisma.$transaction(async(tx)=>{
-        const unsent=await tx.sent.findMany({
-            where:{isSent:false}
+    return shuffled.slice(0, 5);
+  }
+
+  const finder = await prisma.$transaction(async (tx) => {
+    const unsent = await tx.userFounderStatus.findMany({
+      where: { isSent: false }
+    });
+
+    if (unsent.length === 0) {
+      return "you are doing good,but come back later for more emails";
+    }
+
+    const randomTen = getRandomTen(unsent);
+    // removed doing it true for isSent here it should be done in 
+    // the nodemailer thing
+    const emails = await Promise.all(
+      randomTen.map((e: any) =>
+        tx.founder.findUnique({
+          where: {
+            id: e.founderId
+          },
+          select: {
+            email: true,
+            ctoName:true,
+            companyName:true
+          }
         })
-        if(unsent.length===0){
-            return "you are doing good,but come back later for more emails"
-        }
-        const randomTen = getRandomTen(unsent);
-        await tx.sent.updateMany({
-            where:{
-                id:{
-                    in:randomTen.map((e:any)=>e.id)
-                }
-            },
-            data:{
-                isSent:true
-            }
-        })
-        return randomTen
-    })
-    return finder
+      )
+    );
+    return emails;
+  });
+
+  return finder;
 }
